c7b363a2b59598a7ab9a83f4baa668720db93939
Isabel Börkel <isabel_boerkel@yahoo.de>
2019-03-26 20:55:11 +0100

    Auto stash before merge of "master" and "origin/master"
    category JPA

---
+++ src/main/java/geburtststag/jpa/category.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geburtststag.jpa;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.TableGenerator;
+
+/**
+ *
+ * @author Isabel
+ */
+public class category implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "category_ids")
+    @TableGenerator(name = "category_ids", initialValue = 0, allocationSize = 10)
+    private long id;
+    
+    private String name;
+    
+    @OneToMany(mappedBy = "category")
+    List<Geburtstag> geburtstage = new ArrayList<>();
+    
+}
