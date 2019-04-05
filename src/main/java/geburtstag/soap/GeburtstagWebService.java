1d3d9139bcd97ba703d9bd81b1fb5ca57527a6ea
Beliz Balim <belizbalim94@gmail.com>
2019-04-05 13:01:39 +0200

    WebService

--- src/main/java/geburtstag/soap/GeburtstagWebService.java
+++ src/main/java/geburtstag/soap/GeburtstagWebService.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package geburtstag.soap;
-import administration.ejb.SignUpBean;
 import geburtstag.ejb.CategoryBean;
 import geburtstag.ejb.GeburtstagBean;
 import geburtstag.jpa.Category;
@@ -16,8 +15,6 @@ import javax.jws.WebMethod;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 import administration.ejb.UserBean;
-import administration.jpa.User;
-import java.sql.Date;
 import javax.jws.WebParam;
 
 /**
@@ -40,34 +37,25 @@ public class GeburtstagWebService {
     @WebMethod
     @WebResult(name="geburtstage")
     public List<Geburtstag> getAllGeburtstage(
-            @WebParam(name = "username", header = true) String username,
-            @WebParam(name = "password", header = true) String password)
-            throws UserBean.InvalidCredentialsException {
+            @WebParam(name = "username") String username,
+            @WebParam(name = "password") String password)
+            throws UserBean.InvalidCredentialsException, 
+            UserBean.AccessRestrictedException {
         
         this.userBean.validateUser(username, password, "app-user");
         
         return this.gb.findAll();
     }
-    
-    @WebMethod
-    @WebResult(name="geburtstage")
-    public List<Geburtstag> getGeburtstagByDate(
-            @WebParam(name = "username", header = true) String username,
-            @WebParam(name = "password", header = true) String password,
-            @WebParam(name = "date", header = false) Date date) throws UserBean.InvalidCredentialsException {
-        
-        this.userBean.validateUser(username, password, "app-user");
-        return this.gb.findByDate(date);
-    }
 
     @WebMethod
     @WebResult(name="categories")
     public List<Category> getAllCategories(
-            @WebParam(name = "username", header = true) String username,
-            @WebParam(name = "password", header = true) String password) {
+            @WebParam(name = "username") String username,
+            @WebParam(name = "password") String password) 
+            throws UserBean.InvalidCredentialsException, 
+            UserBean.AccessRestrictedException {
         
         this.userBean.validateUser(username, password, "app-user");
-        
         return this.categoryBean.findAll();
     }
 
